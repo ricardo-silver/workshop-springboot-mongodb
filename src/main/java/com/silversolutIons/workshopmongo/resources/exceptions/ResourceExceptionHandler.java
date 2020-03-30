e3d4950dd1f3b8f2dff717bca6f4d6e8e347b965
ricardo-silver <ricardo_override@hotmail.com>
2020-03-30 16:31:04 -0300

    FindById

---
+++ src/main/java/com/silversolutIons/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.silversolutIons.workshopmongo.resources.exceptions;
+
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.silversolutIons.workshopmongo.services.exception.ObjectNotFoundException;
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(),"Não Enecontrado!",e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+				}
+	
+
+}
